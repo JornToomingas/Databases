fabda3ca75e875bdb67ca357f5b6747cf70fb4e7
Jornn <jmarkus@hkhk.edu.ee>
2026-08-23 17:24:01 +0300

    Update crud.php

--- car_rent/crud.php
+++ car_rent/crud.php
@@ -1,34 +1,75 @@
-// CREATE - lisa
-$nimi = "Karl";
+<?php
 
+// muutujad
+$nimi = "Karl";
+$vanus = 18;
+
+echo $nimi;
+echo "Tere " . $nimi;
+
+
+// if
+if ($vanus >= 18) {
+    echo "Täiskasvanu";
+} else {
+    echo "Alaealine";
+}
+
+
+// vormist
+$nimi = $_POST["nimi"];
+$id = $_POST["id"];
+
+
+// array
+$autod = ["BMW", "Audi", "Toyota"];
+
+
+// foreach
+foreach ($autod as $auto) {
+    echo $auto;
+}
+
+
+// CRUD
+
+// lisa
 $sql = "INSERT INTO kasutajad (nimi) VALUES ('$nimi')";
 $conn->query($sql);
 
 
-// READ - näita
+// näita
 $sql = "SELECT * FROM kasutajad";
 $result = $conn->query($sql);
 
 while ($row = $result->fetch_assoc()) {
-    echo $row["id"] . " - " . $row["nimi"] . "<br>";
+    echo $row["nimi"];
 }
 
 
-// UPDATE - muuda
-$id = 1;
-$nimi = "Jaan";
-
+// muuda
 $sql = "UPDATE kasutajad SET nimi='$nimi' WHERE id=$id";
 $conn->query($sql);
 
 
-// DELETE - kustuta
-$id = 1;
-
+// kustuta
 $sql = "DELETE FROM kasutajad WHERE id=$id";
 $conn->query($sql);
 
 
+// põhilised asjad
+
+// $ = muutuja
+// echo = näita
+// . = ühenda tekst
+// == = võrdne
+// === = täpselt võrdne
+// > = suurem
+// < = väiksem
+// && = ja
+// || = või
+
+?>
 
 CREATE → INSERT
 READ   → SELECT
